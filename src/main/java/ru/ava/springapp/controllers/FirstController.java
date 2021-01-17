5836fd76c1d2f827409cceaf1c95296e6774d3f2
Anna <anabrikn@gmail.com>
2021-01-17 23:34:22 +0300

    Model-View. Calculator

--- src/main/java/ru/ava/springapp/controllers/FirstController.java
+++ src/main/java/ru/ava/springapp/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.ava.springapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +10,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") Double a,
+                            @RequestParam("b") Double b,
+                            @RequestParam("action") String action,
+                            Model model) {
+        Double result = null;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / b;
+                break;
+        }
+        model.addAttribute("answer", result);
+        return "first/calculator";
+    }
+
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
